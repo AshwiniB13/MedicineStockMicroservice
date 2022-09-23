757a763be2b46227d7f64564cf638506fbde2aca
ashwini_basavaraju <ashwini_basavaraju@cms.co.in>
2022-09-23 15:36:23 +0530

    commit

---
+++ Controllers/MedicineStockController.cs
@@ -0,0 +1,42 @@
+﻿using CSPharmacyMedicineSupplyManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSMedicineStockMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicineStockController : ControllerBase
+    {
+        private readonly PharmacyMedicineSupplyManagementContext db;
+
+        public MedicineStockController(PharmacyMedicineSupplyManagementContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet] //Fetch Data
+        public async Task<ActionResult> GetMedicineFromStock()
+        {
+            return Ok(await db.MedicineStocks.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddMedicineToStock(MedicineStock p)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MedicineStocks.Add(p);
+                await db.SaveChangesAsync();
+                return Ok(p);
+            }
+            else
+                return BadRequest();
+        }
+    }
+}
---
+++ Models/MedicineStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineStockMicroservice.Models
+{
+    public class MedicineStockTemp
+    {
+        public string MedicineName { get; set; }
+        public string ChemicalComposition { get; set; }
+        public string TargetAilment { get; set; }
+        public DateTime DateOfExpiry { get; set; }
+        public int NumberOfTabletsInStock { get; set; }
+
+        List<MedicineStockTemp> medicineStock = new List<MedicineStockTemp>();
+        public MedicineStockTemp(string medicineName, string chemicalComposition, string targetAilMent, DateTime dateOfExpiry,int noOfTabletsInStock)
+        {
+            MedicineName = medicineName;
+            ChemicalComposition = chemicalComposition;
+            TargetAilment = targetAilMent;
+            DateOfExpiry = dateOfExpiry;
+            NumberOfTabletsInStock = noOfTabletsInStock;
+        }
+
+        public List<MedicineStockTemp> getMedicine()
+        {
+            medicineStock.Add(new MedicineStockTemp("aaa", "qwe","ortho", DateTime.Now.AddYears(5), 100));
+            medicineStock.Add(new MedicineStockTemp("bbb", "wer", "gynoe", DateTime.Now, 980));
+            medicineStock.Add(new MedicineStockTemp("vvv", "rty", "gynoe", DateTime.Now, 370));
+            medicineStock.Add(new MedicineStockTemp("ddd", "tgb","gynoe", DateTime.Now, 500));
+            medicineStock.Add(new MedicineStockTemp("fff", "ert","general", DateTime.Now, 340));
+            return medicineStock;
+        }
+    }
+}
